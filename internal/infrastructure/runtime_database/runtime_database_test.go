ddacd169dda7ffbf2fb139ef258cbcfbaa9c281d
Cubaki <vera.stryka@gmail.com>
2022-10-03 21:50:58 +0300

    fix tests for runtime-database

--- internal/infrastructure/runtime_database/runtime_database_test.go
+++ internal/infrastructure/runtime_database/runtime_database_test.go
@@ -1,6 +1,7 @@
 package runtime_database
 
 import (
+	"fmt"
 	"testing"
 	"tgbot/internal/models"
 	"tgbot/internal/models/models_types"
@@ -10,52 +11,6 @@ import (
 )
 
 func TestRuntimeDatabase(t *testing.T) {
-	t.Run("Testing Add func", func(t *testing.T) {
-		database := NewDatabase()
-		testTitle := models.Title{
-			TitleName:  "TestName",
-			URL:        "TestURL",
-			LastUpdate: time.Now(),
-		}
-
-		database.Add(testTitle)
-
-		require.Equal(t, database.incr.Uint32(), uint32(2))
-		require.Equal(t, database.db[1], testTitle)
-	})
-
-	t.Run("Testing Delete func", func(t *testing.T) {
-		t.Run("Delete title with correct name", func(t *testing.T) {
-			database := NewDatabase()
-			testTitle := models.Title{
-				TitleName:  "TestName",
-				URL:        "TestURL",
-				LastUpdate: time.Now(),
-			}
-			database.Add(testTitle)
-
-			err := database.Delete(testTitle.TitleName)
-
-			require.NoError(t, err)
-			require.Equal(t, len(database.db), 0)
-		})
-		t.Run("Delete title with wrong name", func(t *testing.T) {
-			database := NewDatabase()
-			testTitle := models.Title{
-				TitleName:  "TestName",
-				URL:        "TestURL",
-				LastUpdate: time.Now(),
-			}
-			database.Add(testTitle)
-			var wrongTestName models_types.TitleName = "wrongTestName"
-
-			err := database.Delete(wrongTestName)
-
-			require.EqualError(t, err, "title with such name doesn't exist in database")
-			require.Equal(t, len(database.db), 1)
-		})
-	})
-
 	t.Run("Testing of List func", func(t *testing.T) {
 		database := NewDatabase()
 		testTitle := models.Title{
@@ -71,6 +26,54 @@ func TestRuntimeDatabase(t *testing.T) {
 		}
 		actMap := database.List()
 
-		require.Equal(t, actMap, expMap)
+		require.Equal(t, expMap, actMap)
+	})
+
+	t.Run("Testing Add func", func(t *testing.T) {
+		database := NewDatabase()
+		testTitle := models.Title{
+			TitleName:  "TestName",
+			URL:        "TestURL",
+			LastUpdate: time.Now(),
+		}
+		expIncr := database.incr + 1
+		database.Add(testTitle)
+
+		require.Equal(t, expIncr, database.incr)
+		require.Equal(t, testTitle, database.db[expIncr-1])
+	})
+
+	t.Run("Testing Delete func", func(t *testing.T) {
+		t.Run("Delete title with correct name", func(t *testing.T) {
+			database := NewDatabase()
+			testTitle := models.Title{
+				TitleName:  "TestName",
+				URL:        "TestURL",
+				LastUpdate: time.Now(),
+			}
+			database.Add(testTitle)
+			expLen := len(database.db) - 1
+
+			err := database.Delete(testTitle.TitleName)
+			fmt.Println(database.db)
+			require.NoError(t, err)
+			require.Equal(t, expLen, len(database.db))
+		})
+		t.Run("Delete title with wrong name", func(t *testing.T) {
+			database := NewDatabase()
+			testTitle := models.Title{
+				TitleName:  "TestName",
+				URL:        "TestURL",
+				LastUpdate: time.Now(),
+			}
+			database.Add(testTitle)
+			expLen := len(database.db)
+			var wrongTestName models_types.TitleName = "wrongTestName"
+
+			err := database.Delete(wrongTestName)
+
+			require.EqualError(t, err, "title with such name doesn't exist in database")
+			require.Equal(t, expLen, len(database.db))
+		})
 	})
 }
